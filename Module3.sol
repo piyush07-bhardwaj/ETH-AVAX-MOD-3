c168be93a0189bde339e6b0291021fef9793d41f
piyush07-bhardwaj <146122258+piyush07-bhardwaj@users.noreply.github.com>
2024-06-25 12:42:42 +0530

    Update Module3.sol

--- Module3.sol
+++ Module3.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract SimpleToken is ERC20 {
-constructorERC20("Simple", "SIMP")
+constructor() 
+ERC20("Simple", "SIMP")
 {
 _mint(msg.sender, 100 * 10 ** decimals());
   }
